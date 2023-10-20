7e789df6743bdc7d5dbb6cad62f32568a4f04a69
M-Farmaha <115003821+M-Farmaha@users.noreply.github.com>
2023-10-20 21:45:47 +0300

    backend - auth

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+
+import authRouter from "./routes/api/auth-router.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/users", authRouter);
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({
+    message,
+  });
+});
+
+export default app;
